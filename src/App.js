627a4e1d87921bd64e78f3a3dbd645a620ababdd
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-05-12 14:54:21 +0200

    Added color and font variables
    + assets folder, and files such as logo, icons and images.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./sass/style.scss"
 
 function App() {
   return (
+    <>
     <h1>Hello World</h1>
+<p>Nostrud mollit laboris cupidatat aliqua sit. Cillum nostrud et commodo enim dolore officia quis minim aliqua. Anim nisi veniam magna dolore pariatur nulla mollit proident nostrud. Dolor officia adipisicing mollit excepteur velit pariatur nulla dolor laborum dolor do. Ea irure eu proident cupidatat id deserunt velit officia.</p>
+    </>
   );
 }
 
